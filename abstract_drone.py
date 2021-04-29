36613f9782877d3ee278c075f4ac1eacc59e86f0
mvbraga@gmail.com <marvinbraga>
2021-04-29 01:30:39 -0300

    Controles do Drone

---
+++ abstract_drone.py
@@ -0,0 +1,165 @@
+# coding=utf-8
+"""
+Módulo para Drone Abstrato.
+"""
+import contextlib
+import socket
+import threading
+import time
+from abc import ABCMeta, abstractmethod
+from threading import Event, Thread
+
+from drone_manager import logger
+
+
+class DroneManagerNotFound(Exception):
+    """ Classe para exceção de Drone Manager não encontrado. """
+
+    def __init__(self):
+        super(DroneManagerNotFound, self).__init__(
+            'Antes de executar o patrulhamento você deve informar um AbstractDroneManager.')
+
+
+class AbstractPatrolMiddleware(metaclass=ABCMeta):
+    """ Classe abstrata para implementações de regras de patrulhamento """
+
+    def __init__(self, next_middleware=None):
+        self._drone_manager = None
+        self._next_middleware = next_middleware
+
+    @abstractmethod
+    def _process(self, status, *args, **kwargs):
+        return 0
+
+    def process(self, status, *args, **kwargs):
+        """ Método para executar processamento de patrulhamento """
+        if not self._drone_manager:
+            raise DroneManagerNotFound()
+
+        process_id = self._process(status, args, kwargs)
+        if self._next_middleware:
+            self._next_middleware.process(status, args, kwargs)
+        return process_id
+
+    def set_drone_manager(self, value):
+        """ Informa o gerenciador do drone para todos os middlewares vinculados. """
+        self._drone_manager = value
+        if self._next_middleware:
+            self._next_middleware.set_drone_manager(value)
+        return self
+
+
+class AbstractDroneManager(metaclass=ABCMeta):
+    """ Classe para gerenciamento do drone. """
+
+    def __init__(self, host_ip, host_port, drone_ip, drone_port, is_imperial, speed, middleware):
+        self.middleware = middleware
+        self.speed = speed
+        self.is_imperial = is_imperial
+        self.drone_port = drone_port
+        self.drone_ip = drone_ip
+        self.host_port = host_port
+        self.host_ip = host_ip
+        self.drone_address = (drone_ip, drone_port)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((self.host_ip, self.host_port))
+        self.response = None
+        # Patrol
+        self.patrol_event = None
+        self.is_patrol = False
+        self._patrol_semaphore = threading.Semaphore()
+        self._thread_patrol = None
+        # Stop
+        self.stop_event = Event()
+        self._response_thread = Thread(target=self.receive_response, args=(self.stop_event,))
+        self._response_thread.start()
+        self._init_commands()
+
+    @abstractmethod
+    def _init_commands(self):
+        pass
+
+    def __del__(self):
+        self.stop()
+
+    def _retry(self, check_method, iter_number):
+        """ Método para fazer tentativas em uma checagem.  """
+        retry = 0
+        while check_method():
+            time.sleep(0.3)
+            if retry > iter_number:
+                break
+            retry += 1
+        return self
+
+    def _is_none_response(self):
+        """ Verifica se existe uma response. """
+        return self.response is None
+
+    def receive_response(self, stop_event):
+        """ Info """
+        while not stop_event.is_set():
+            try:
+                self.response, ip = self.socket.recvfrom(3000)
+                logger.info({'action': 'receive_response', 'response': self.response})
+            except socket.error as e:
+                logger.error({'action': 'receive_response', 'error': e})
+                break
+
+    def stop(self):
+        """ Fecha a conexão """
+        self.stop_event.set()
+        self._retry(self._response_thread.is_alive, 30)
+        self.socket.close()
+
+    def send_command(self, command):
+        """ Registrar o envio de um comando. """
+        logger.info({'action': 'send_command', 'command': command})
+        self.socket.sendto(command.encode('utf-8'), self.drone_address)
+
+        self._retry(self._is_none_response, 3)
+
+        response = None
+        if not self._is_none_response():
+            response = self.response.decode('utf-8')
+
+        self.response = None
+        return response
+
+    def patrol(self):
+        """ Inicializa as condições para fazer o patrulhamento. """
+        if not self.is_patrol:
+            self.patrol_event = Event()
+            self._thread_patrol = Thread(
+                target=self._patrol,
+                args=(self._patrol_semaphore, self.patrol_event, self.middleware,)
+            )
+            self.is_patrol = True
+            self._thread_patrol.start()
+        return self
+
+    def stop_patrol(self):
+        """ Método para parar o patrulhamento. """
+        if self.is_patrol:
+            try:
+                self.patrol_event.set()
+                self._retry(self._thread_patrol.is_alive, 300)
+            finally:
+                self.is_patrol = False
+        return self
+
+    @staticmethod
+    def _patrol(semaphore, stop_event, middleware):
+        """ Método para executar o patrulhamento. """
+        is_acquire = semaphore.acquire(blocking=False)
+        if is_acquire:
+            logger.info({'action': '_patrol', 'status': 'acquire'})
+            with contextlib.ExitStack() as stack:
+                stack.callback(semaphore.release)
+                status = 0
+                while not stop_event.is_set():
+                    status += 1
+                    if middleware:
+                        status = middleware.process(status)
+        else:
+            logger.warning({'action': '_patrol', 'status': 'not_acquire'})
--- drone_manager.py
+++ drone_manager.py
@@ -3,11 +3,11 @@
 Módulo de conexão com o Tello Drone.
 """
 import logging
-import socket
 import sys
 import time
-from abc import ABCMeta, abstractmethod
-from threading import Thread, Event
+from enum import Enum
+
+from abstract_drone import AbstractDroneManager, AbstractPatrolMiddleware
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 logger = logging.getLogger(__name__)
@@ -17,100 +17,174 @@ logger = logging.getLogger(__name__)
 # STATE_PORT = 8890
 # VIDEO_STREAM_PORT = 11111
 
-
-class BaseDroneManager(metaclass=ABCMeta):
-    """ Classe para gerenciamento do drone. """
-
-    def __init__(self, host_ip, host_port, drone_ip, drone_port):
-        self.drone_port = drone_port
-        self.drone_ip = drone_ip
-        self.host_port = host_port
-        self.host_ip = host_ip
-        self.drone_address = (drone_ip, drone_port)
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.bind((self.host_ip, self.host_port))
-        self.response = None
-        self.stop_event = Event()
-        self._response_thread = Thread(target=self._receive_response, args=(self.stop_event,))
-        self._response_thread.start()
-        self._init_commands()
-
-    @abstractmethod
-    def _init_commands(self):
-        pass
-
-    def __del__(self):
-        self.stop()
-
-    def _retry(self, check_method, iter_number):
-        """ Método para fazer tentativas em uma checagem.  """
-        retry = 0
-        while check_method():
-            time.sleep(0.3)
-            if retry > iter_number:
-                break
-            retry += 1
-        return self
-
-    def _is_none_response(self):
-        """ Verifica se existe uma response. """
-        return self.response is None
-
-    def _receive_response(self, stop_event):
-        """ Info """
-        while not stop_event.is_set():
-            try:
-                self.response, ip = self.socket.recvfrom(3000)
-                logger.info({'action': '_receive_response', 'response': self.response})
-            except socket.error as e:
-                logger.error({'action': '_receive_response', 'error': e})
-                break
-
-    def stop(self):
-        """ Fecha a conexão """
-        self.stop_event.set()
-        self._retry(self._response_thread.is_alive, 30)
-        self.socket.close()
-
-    def send_command(self, command):
-        """ Registrar o envio de um comando. """
-        logger.info({'action': 'send_command', 'command': command})
-        self.socket.sendto(command.encode('utf-8'), self.drone_address)
-
-        self._retry(self._is_none_response, 3)
-
-        response = None
-        if not self._is_none_response():
-            self.response.decode('utf-8')
-
-        self.response = None
-        return response
+class TelloFlipPosition(Enum):
+    """ Posições para o drone fazer flip. """
+    left = 'i'
+    right = 'r'
+    forward = 'f'
+    back = 'b'
 
 
-class TelloDrone(BaseDroneManager):
+class TelloDrone(AbstractDroneManager):
     """ Classe Específica para o Drone Tello. """
+    DEFAULT_DISTANCE = 0.30
+    DEFAULT_SPEED = 10
+    DEFAULT_DEGREE = 10
 
-    def __init__(self, host_ip='192.168.10.2', host_port=8889, drone_ip='192.168.10.1', drone_port=8889):
-        super(TelloDrone, self).__init__(host_ip, host_port, drone_ip, drone_port)
+    def __init__(self, host_ip='192.168.10.3', host_port=8889, drone_ip='192.168.10.1', drone_port=8889,
+                 is_imperial=False, speed=DEFAULT_SPEED, middleware=None):
+        super(TelloDrone, self).__init__(host_ip, host_port, drone_ip, drone_port, is_imperial, speed, middleware)
 
     def _init_commands(self):
         self.send_command('command')
         self.send_command('streamon')
+        self.set_speed(self.speed)
 
     def takeoff(self):
         """ Info """
         self.send_command('takeoff')
+        return self
 
     def land(self):
         """ Voltar para o chão. """
         self.send_command('land')
+        return self
+
+    def _move(self, direction, distance):
+        """ Método para movimentar o drone em certa direção e distância. """
+        distance = float(distance)
+        distance = int(round(distance * 30.48)) if self.is_imperial else int(round(distance * 100))
+        return self.send_command(f'{direction} {distance}')
+
+    def up(self, distance=DEFAULT_DISTANCE):
+        """ Método para subir o drone. """
+        self._move('up', distance)
+        return self
+
+    def down(self, distance=DEFAULT_DISTANCE):
+        """ Método para descer o drone. """
+        self._move('down', distance)
+        return self
+
+    def left(self, distance=DEFAULT_DISTANCE):
+        """ Método para subir o drone. """
+        self._move('left', distance)
+        return self
+
+    def right(self, distance=DEFAULT_DISTANCE):
+        """ Método para subir o drone. """
+        self._move('right', distance)
+        return self
+
+    def forward(self, distance=DEFAULT_DISTANCE):
+        """ Método para subir o drone. """
+        self._move('forward', distance)
+        return self
+
+    def back(self, distance=DEFAULT_DISTANCE):
+        """ Método para subir o drone. """
+        self._move('back', distance)
+        return self
+
+    def set_speed(self, speed):
+        """ Método para ajustar a velocidade do drone. """
+        self.send_command(f'speed {speed}')
+        return self
+
+    def clockwise(self, degree=DEFAULT_DEGREE):
+        """ Girar no sentido horário. """
+        self.send_command(f'cw {degree}')
+        return self
+
+    def count_clockwise(self, degree=DEFAULT_DEGREE):
+        """ Girar no sentido anti-horário. """
+        self.send_command(f'ccw {degree}')
+        return self
+
+    def _flip(self, position: TelloFlipPosition):
+        """ Método para executar o flip. """
+        self.send_command(f'flip {position.value}')
+        return self
+
+    def flip_left(self):
+        """ Executa o flip para a esquerda. """
+        self._flip(TelloFlipPosition.left)
+        return self
+
+    def flip_right(self):
+        """ Executa o flip para a direita. """
+        self._flip(TelloFlipPosition.right)
+        return self
+
+    def flip_forward(self):
+        """ Executa o flip para a frente. """
+        self._flip(TelloFlipPosition.forward)
+        return self
+
+    def flip_back(self):
+        """ Executa o flip para a trás. """
+        self._flip(TelloFlipPosition.back)
+        return self
+
+
+class BasicPatrolMiddleware(AbstractPatrolMiddleware):
+    """ Faz o patrulhamento básico. """
+
+    def _process(self, status, *args, **kwargs):
+        process_id = status
+        if process_id == 1:
+            self._drone_manager.up()
+        elif process_id == 2:
+            self._drone_manager.clockwise(90)
+        elif process_id == 3:
+            self._drone_manager.down()
+        elif process_id == 4:
+            process_id = 0
+        time.sleep(3)
+        return process_id
 
 
 if __name__ == '__main__':
-    drone_manager = TelloDrone()
+    middleware = BasicPatrolMiddleware()
+    drone_manager = TelloDrone(middleware=middleware)
     try:
-        drone_manager.takeoff()
+        middleware.set_drone_manager(drone_manager)
+        drone_manager.set_speed(100).takeoff()
+        time.sleep(7)
+        drone_manager.clockwise(90).clockwise(90).clockwise(90).clockwise(90)
+        time.sleep(3)
+        drone_manager.count_clockwise(90).count_clockwise(90).count_clockwise(90).count_clockwise(90)
+        time.sleep(3)
+
+        drone_manager.forward()
         time.sleep(5)
+        drone_manager.right()
+        time.sleep(5)
+        drone_manager.back()
+        time.sleep(5)
+        drone_manager.left()
+        time.sleep(5)
+
+        drone_manager.set_speed(10)
+        time.sleep(1)
+        drone_manager.up()
+        time.sleep(5)
+        drone_manager.down()
+        time.sleep(5)
+
+        drone_manager.flip_left()
+        time.sleep(3)
+        drone_manager.flip_right()
+        time.sleep(3)
+        drone_manager.flip_forward()
+        time.sleep(3)
+        drone_manager.flip_back()
+        time.sleep(3)
+
+        drone_manager.patrol()
+        time.sleep(45)
+        drone_manager.stop_patrol()
+
         drone_manager.land()
     finally:
         drone_manager.stop()
